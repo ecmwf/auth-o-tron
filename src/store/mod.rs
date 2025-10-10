d922ceab4bac6ebb5cd3740a5a15079a4ddc9cb6
sametd <sametdemir@gmail.com>
2025-10-11 00:05:22 +0200

    module renaming

--- src/store/mod.rs
+++ src/store/mod.rs
@@ -1,7 +1,7 @@
+pub mod base;
 pub mod mongodb_store;
 pub mod no_store;
-pub mod store;
 
 // Re-export the primary Store items so code outside can do
 // "use crate::store::{Store, create_store};"
-pub use store::{Store, create_store};
+pub use base::{Store, create_store};
