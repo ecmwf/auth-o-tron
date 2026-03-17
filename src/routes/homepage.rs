a0f04f380f3fb8f1c06f2635242638f4be7a8791
sametd <sametdemir@gmail.com>
2026-03-17 11:01:54 +0100

    feat(homepage): use logo-light variant, add favicon, rename Browser to Client
    Switch embedded logo from logo.png to logo-light.png. Serve favicon.ico
    with correct image/x-icon MIME type. Fix architecture diagram label
    from Browser to Client.

--- src/routes/homepage.rs
+++ src/routes/homepage.rs
@@ -2,12 +2,14 @@ use crate::state::AppState;
 use axum::{Router, http::header, response::IntoResponse, routing::get};
 
 const TEMPLATE: &str = include_str!("../static/index.html");
-const LOGO: &[u8] = include_bytes!("../../docs/logo.png");
+const LOGO: &[u8] = include_bytes!("../../docs/logo-light.png");
+const FAVICON: &[u8] = include_bytes!("../../docs/favicon.ico");
 
 pub fn routes() -> Router<AppState> {
     Router::new()
         .route("/", get(index))
         .route("/static/logo.png", get(logo))
+        .route("/favicon.ico", get(favicon))
 }
 
 async fn index() -> impl IntoResponse {
@@ -17,4 +19,8 @@ async fn index() -> impl IntoResponse {
 
 async fn logo() -> impl IntoResponse {
     ([(header::CONTENT_TYPE, "image/png")], LOGO)
+}
+
+async fn favicon() -> impl IntoResponse {
+    ([(header::CONTENT_TYPE, "image/x-icon")], FAVICON)
 }
